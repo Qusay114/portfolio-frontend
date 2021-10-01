87c0aeed54f275bd2625a32eae4695b0a2b55faf
Qusay114 <eng.qusay015@gmail.com>
2021-10-01 23:45:51 +0300

    add tab icon

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -6,6 +6,10 @@ import { Button , Modal , Form} from 'react-bootstrap' ;
 export default function ContactForm(props){
 
   const {REACT_APP_EMAIL_SERVICE_ID , REACT_APP_EMAIL_USER_ID , REACT_APP_EMAIL_TEMPLATE_ID} = process.env ;
+  console.log.log(REACT_APP_EMAIL_USER_ID) ; 
+  console.log.log(REACT_APP_EMAIL_SERVICE_ID) ; 
+  console.log.log(REACT_APP_EMAIL_TEMPLATE_ID) ; 
+
 
   const success = (e) => {
     e.preventDefault();
